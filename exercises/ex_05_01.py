00237aca676fb243b89b1e790622c5b641e95924
abeflores <abeflores.data@gmail.com>
2023-08-04 12:44:06 -0700

    new exercise

---
+++ exercises/ex_05_01.py
@@ -0,0 +1,13 @@
+num = 0
+tot = 0
+
+while True:
+    sval = input('Enter a number: ')
+    if sval == 'done':
+        break
+    fval = float(sval)
+    print(fval)
+    num = num + 1
+    tot = tot + fval
+
+print(num, tot, tot/num)

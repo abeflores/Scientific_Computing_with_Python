985684fb6ec60843762b55a1184dacafad51437e
abeflores <abeflores.data@gmail.com>
2023-09-15 10:27:13 -0700

    new exercise

---
+++ exercises/ex_10_01.py
@@ -0,0 +1,39 @@
+fname = input('Enter file name: ')
+if len(fname) < 1:
+    fname = 'clown.txt'
+hand = open(fname)
+
+
+di = dict()
+
+
+for lin in hand:
+    lin = lin.rstrip()
+    # print(lin)
+    wds = lin.split()
+    # print(wds)
+    for w in wds:
+
+        # oldcount = di.get(w, 0)
+        # print(w, 'old', oldcount)
+        # newcount = oldcount + 1
+        # di[w] = newcount
+        # print(w, 'new', newcount)
+
+        di[w] = di.get(w, 0) + 1
+
+        # print(w, 'new', di[w])
+
+# print(di)
+
+x = di.items()
+print(x)
+
+largest = -1
+for k, v in di.items():
+    # print(k, v)
+    if v > largest:
+        largest = v
+        theword = k
+
+# print('Done', theword, largest)
